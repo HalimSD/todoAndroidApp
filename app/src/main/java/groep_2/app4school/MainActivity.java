becd407e605b846e9d94baeee8a6ac46a8c16c87
halim <a.alhalim94@gmail.com>
2018-05-30 10:06:47 +0200

    adding the google calendar API and login service

--- app/src/main/java/groep_2/app4school/MainActivity.java
+++ app/src/main/java/groep_2/app4school/MainActivity.java
@@ -1,10 +1,8 @@
 package groep_2.app4school;
 
-import android.annotation.SuppressLint;
-import android.database.sqlite.SQLiteException;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.internal.NavigationMenuItemView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,35 +11,54 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+import com.google.android.gms.common.SignInButton;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import groep_2.app4school.database.DataSource;
 import groep_2.app4school.model.todoItem;
 import groep_2.app4school.sample.SampleDataProvider;
+import pub.devrel.easypermissions.EasyPermissions;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, EasyPermissions.PermissionCallbacks {
     private DrawerLayout drawer;
-    TextView tvOut;
+//    TextView tvOut;
     List<todoItem> todoItemList = SampleDataProvider.dataItemList;
-    List<String> todoTitles = new ArrayList<>();
+//    List<String> todoTitles = new ArrayList<>();
     DataSource mDataSource;
+    public Button mButton;
+    public FirebaseAuth mAuth;
+    public Button accbtn;
 
-    @SuppressLint("ResourceType")
+
+
+
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
+
+    private static final String BUTTON_TEXT = "Call Google Calendar API";
+    private static final String PREF_ACCOUNT_NAME = "accountName";
+    private static final String[] SCOPES = {CalendarScopes.CALENDAR_READONLY};
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+
+
         setContentView(R.layout.activity_main);
-
-//        todoItem item = new todoItem(null, "todo title1", "first todo", "high");
-//        tvOut = (TextView) findViewById(R.id.out);
-//        tvOut.setText("");
-
         mDataSource = new DataSource(this);
         mDataSource.open();
         mDataSource.insertDatabase(todoItemList);
@@ -51,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         DataItemAdapter adapter = new DataItemAdapter(this, ListFromDB);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(android.R.id.list);
+        RecyclerView recyclerView = findViewById(android.R.id.list);
         recyclerView.setAdapter(adapter);
 
 
@@ -67,11 +84,43 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction().replace(R.id.drawer_layout,
-//                    new Done()).commit();
-//            navigationView.setCheckedItem(R.id.nav_done);
+    //        if (savedInstanceState == null) {
+    //            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+    //                    new Done()).commit();
+    //            navigationView.setCheckedItem(R.id.fragment_container);
+    //        }
+
+        mButton = findViewById(R.id.callApi);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCallApiActivity();
+            }
+        });
+    }
+
+    public void openCallApiActivity() {
+        Intent intent = new Intent(this, callApi.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null){
+            sendToLogin();
+        }
+// else {
+//            Toast.makeText(this, "you are signed in", Toast.LENGTH_SHORT).show();
 //        }
+    }
+
+    private void sendToLogin() {
+        Intent loginIntent = new Intent(this , Account.class);
+       startActivity(loginIntent);
+        finish();
     }
 
     @Override
@@ -99,42 +148,72 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_highPriority:
-                getSupportFragmentManager().beginTransaction().replace(R.id.drawer_layout,
-                    new High_priority()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new High_priority()).addToBackStack(null).commit();
                 break;
             case R.id.nav_noPriority:
-                getSupportFragmentManager().beginTransaction().replace(R.id.drawer_layout,
-                        new No_priority()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new No_priority()).addToBackStack(null).commit();
                 break;
             case R.id.nav_lowPriority:
-                getSupportFragmentManager().beginTransaction().replace(R.id.drawer_layout,
-                        new Low_priority()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Low_priority()).addToBackStack(null).commit();
                 break;
             case R.id.nav_3days:
-                getSupportFragmentManager().beginTransaction().replace(R.id.drawer_layout,
-                        new days()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new days()).addToBackStack(null).commit();
                 break;
             case R.id.nav_week:
-                getSupportFragmentManager().beginTransaction().replace(R.id.drawer_layout,
-                        new week()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new week()).addToBackStack(null).commit();
                 break;
             case R.id.nav_month:
-                getSupportFragmentManager().beginTransaction().replace(R.id.drawer_layout,
-                        new month()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new month()).addToBackStack(null).commit();
                 break;
             case R.id.nav_done:
-                getSupportFragmentManager().beginTransaction().replace(R.id.drawer_layout,
-                        new Done()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Done()).addToBackStack(null).commit();
                 break;
-            case R.id.nav_account:
-                getSupportFragmentManager().beginTransaction().replace(R.id.drawer_layout,
-                        new Account()).commit();
-                break;
+//            case R.id.nav_account:
+//                Intent newActo = new Intent(this, Account.class);
+//                startActivity(newActo);
+//                finish();
+//                break;
 
         }
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+
+    }
+
+    static class ViewHolder {
+        @BindView(R.id.toolbar)
+        Toolbar toolbar;
+        @BindView(android.R.id.list)
+        RecyclerView list;
+//        @BindView(R.id.googleBtn)
+//        SignInButton googleBtn;
+        @BindView(R.id.callApi)
+        Button callApi;
+        @BindView(R.id.nav_view)
+        NavigationView navView;
+        @BindView(R.id.drawer_layout)
+        DrawerLayout drawerLayout;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
     }
 }
 
