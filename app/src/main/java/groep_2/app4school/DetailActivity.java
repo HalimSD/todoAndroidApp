fe49347bb3d0e148ad9bf7db9ae90c3f4fa8ce9c
HalimSD <a.alhalim94@gmail.com>
2018-06-17 13:44:17 +0200

    adding a delete button in the detail activity

--- app/src/main/java/groep_2/app4school/DetailActivity.java
+++ app/src/main/java/groep_2/app4school/DetailActivity.java
@@ -1,14 +1,19 @@
 package groep_2.app4school;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -17,12 +22,18 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import groep_2.app4school.model.todoItem;
+import groep_2.app4school.utils.SharedPrefManager;
+import groep_2.app4school.utils.Util;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class DetailActivity extends AppCompatActivity {
 
     TextView todo_title, todo_description, todo_status, todo_deadline, todo_priority;
     ListView listViewTodo;
+    private String mEmail;
+    SharedPrefManager sharedPrefManager;
+    Context mContext = this;
+    Button deleteDetail;
 
 
     @Override
@@ -36,12 +47,12 @@ public class DetailActivity extends AppCompatActivity {
         todo_deadline = findViewById(R.id.todo_deadline);
         todo_status = findViewById(R.id.todo_status);
         todo_priority = findViewById(R.id.todo_priority);
-
+        FloatingActionButton deleteDetail = findViewById(R.id.deleteDetails);
         listViewTodo = findViewById(R.id.listViewTodo);
 
         Intent intent = getIntent();
 
-        String id = intent.getStringExtra(MainActivity.todo_id);
+        final String id = intent.getStringExtra(MainActivity.todo_id);
         String title = intent.getStringExtra(MainActivity.todo_title);
         todo_title.setText(title);
         String description = intent.getStringExtra(MainActivity.todo_description);
@@ -52,6 +63,24 @@ public class DetailActivity extends AppCompatActivity {
         todo_status.setText(status);
         String priority = intent.getStringExtra(MainActivity.todo_priority);
         todo_priority.setText(priority);
+        sharedPrefManager = new SharedPrefManager(mContext);
+        mEmail = sharedPrefManager.getUserEmail();
 
+
+        deleteDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteTodo(id);
+
+            }
+        });
+
+    }
+    private void deleteTodo(String id) {
+        DatabaseReference dfDelete = FirebaseDatabase.getInstance().getReference("todos").child(Util.encodeEmail(mEmail)).child(id);
+        dfDelete.removeValue();
+        Toast.makeText(this, "The todo is deleted!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
